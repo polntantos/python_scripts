f03b0f6c65c68317184e7100a0b0b5e46f510aa6
polntantos <polykarpos97@gmail.com>
2023-05-05 14:54:16 +0300

    first attempt on spark

---
+++ pyspark_try.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+
+spark = (
+  SparkSession.builder
+  .appName('myRdfSpark.com')
+  .config("spark.driver.extraClassPath","jars/maria/mariadb-java-client-3.1.4.jar")
+  .config("spark.executor.memory", "4g") 
+  .config("spark.driver.memory", "4g") 
+  .master("local")
+  .getOrCreate()
+  )
+
+properties={
+  "driver":"org.mariadb.jdbc.Driver", 
+  "user":"homestead",
+  "password":"secret"
+}
+
+df = spark.read.jdbc(
+  url="jdbc:mariadb://192.168.10.30:3306/homestead",
+  table="products",
+  properties=properties
+) 
+
+df.take(10).show()
+
+    
+    # .option("dbtable","products") \
+    # .option("url", "jdbc:mariadb://192.168.10.30:3306/homestead") \
+# print(type(df))
+# df.show()
