ba3fbf529912ebff4d309a2b87b2eab43fd65fee
polntantos <polykarpos97@gmail.com>
2023-08-02 03:26:45 +0300

    5_2 categorization must proceed for skipped clusters

--- 5_2_categorize_clusters.py
+++ 5_2_categorize_clusters.py
@@ -2,33 +2,35 @@ from classes.VirtuosoWrapper import VirtuosoWrapper
 import json
 import re
 
+
 def filter_keywords(keywords, title):
     filtered_keywords = []
     title = re.sub(r"[>#+'?$%^*®™()]+|-", "", title)
     title_parts = title.lower().split()
     for keyword in keywords:
-      keyword_parts = keyword.split()
-      flag = False
-      for keyword_part in keyword_parts:
-        if keyword_part in title_parts:
-          flag=True
-          continue
-        else:
-          flag=False
-          break
-      if flag:
-        filtered_keywords.append(keyword)
+        keyword_parts = keyword.split()
+        flag = False
+        for keyword_part in keyword_parts:
+            if keyword_part in title_parts:
+                flag = True
+                continue
+            else:
+                flag = False
+                break
+        if flag:
+            filtered_keywords.append(keyword)
 
     return filtered_keywords
 
 
 clusters = []
 
+
 def create_product_category_query(product_uris):
-  uri_array = []
-  for product_uri in product_uris:
-    uri_array.append(f"<{product_uri}>")
-  return f"""SELECT DISTINCT ?o
+    uri_array = []
+    for product_uri in product_uris:
+        uri_array.append(f"<{product_uri}>")
+    return f"""SELECT DISTINCT ?o
   WHERE{{
   ?product <http://magelon.com/ontologies/products#title> ?product_title .
   ?product <http://magelon.com/ontologies/products#product_type> ?o.
@@ -37,26 +39,37 @@ def create_product_category_query(product_uris):
   ))}}
   """
 
-def update_product_google_categories_query(product_uris,category_uri):
-  uri_array = []
-  for product_uri in product_uris:
-    uri_array.append(f"<{product_uri}>")
-  return f"""
+
+def update_product_google_categories_query(product_uris, category_uri):
+    uri_array = []
+    for product_uri in product_uris:
+        uri_array.append(f"<{product_uri}>")
+    return f"""
       INSERT {{
-      ?product <http://omikron44/ontologies/products#google_product_type> <{category_uri}> .
+      ?product <http://magelon.com/ontologies/products#google_product_type> <{category_uri}> .
       }}
       WHERE {{
-      ?product a <http://omikron44/ontologies/products>.
+      ?product a <http://magelon.com/ontologies/products>.
       FILTER(?product IN (
         {",".join(uri_array)}
       ))
       }}
   """
 
+
+def get_category_score(category_name, category_keywords):
+    category_words = category_name.split()
+    category_score = 0
+    for category_word in category_words:
+        if category_word.lower() in " ".join(category_keywords).lower():
+            category_score += len(category_word) / len(name)
+    return category_score
+
+
 with open("storage/agglo-clusters.json", "r") as json_file:
-  clusters = json.load(json_file)
-  
-  
+    clusters = json.load(json_file)
+
+
 virtuoso = VirtuosoWrapper()
 
 categories_query = """
@@ -68,15 +81,6 @@ where {
 }
 """
 
-attributes_query = """
-PREFIX magelon: <http://magelon.com/ontologies/>
-SELECT *
-where{
- ?attribute a magelon:attributes.
- ?attribute rdfs:label ?attribute_label.
-}
-"""
-
 categories_result = virtuoso.getAll(categories_query)
 category_names = {}
 category_paths = {}
@@ -84,85 +88,96 @@ for row in categories_result:
     category_paths[row["category_uri"]] = row["full_path"]
     category_names[row["category_uri"]] = row["category_name"]
 
+remaining_clusters = {}
+
 for i, products in clusters.items():
-    product_uris = [product_uri for product_uris in products["product_uris"] for product_uri in product_uris]
+    product_uris = [
+        product_uri
+        for product_uris in products["product_uris"]
+        for product_uri in product_uris
+    ]
     merchant_category_query = create_product_category_query(product_uris=product_uris)
-    merchant_categories_result= virtuoso.getAll(merchant_category_query)
+    merchant_categories_result = virtuoso.getAll(merchant_category_query)
     merchant_categories = [row["o"].lower() for row in merchant_categories_result]
     category_keywords = []
     for merchant_category in merchant_categories:
-      category_keywords.extend(re.findall(r"\b(\w+-?\w+)\b", merchant_category))
+        category_keywords.extend(re.findall(r"\b(\w+-?\w+)\b", merchant_category))
     print(set(category_keywords))
     category_scores = []
-    for uri,name in category_names.items():
-        category_words = name.split()
-        category_score = 0
-        for category_word in category_words:
-            if(category_word.lower() in " ".join(category_keywords).lower()):
-                category_score +=len(category_word)/len(name)
-        category_scores.append((category_score, name,uri))
+    for uri, name in category_names.items():
+        category_score = get_category_score(name, category_keywords)
+        category_scores.append((category_score, name, uri))
     top_categories = sorted(category_scores, key=lambda x: x[0], reverse=True)
     print("\n")
+    print("Products to receive categories")
     for text in products["product_titles"][0:3]:
-        print("Products to receive categories")
         print(text)
     print("\n")
     print("Category matches")
-    for i,text in enumerate(top_categories[0:5]):
+    for i, text in enumerate(top_categories[0:5]):
         print("\n")
         print(f"{i}:{text[1]}:({category_paths[text[2]]})")
     print("\n")
     selection = input("Check paths and assign if relation is found[0-4]")
-    if(selection != "" and int(selection) in range(0,4)):
-      print(f"selection {top_categories[int(selection)]}")
-      query = update_product_google_categories_query(
-        product_uris,
-        top_categories[int(selection)][2]
-      )
-      print(query)
-      exit()
-      # virtuoso.save(query)
-      #update product google categories
+    if selection != "" and int(selection) in range(0, 5):
+        print(f"selection {top_categories[int(selection)]}")
+        query = update_product_google_categories_query(
+            product_uris, top_categories[int(selection)][2]
+        )
+        # print(query)
+        # update product google categories
+        virtuoso.get(query)
+        continue
     else:
-      print("no selection")
-
-
-for i, products in clusters.items():
-    category_scores = []
-    cluster_keywords = []
-    for product in products["product_titles"]:
-        cluster_keywords.extend(re.findall(r"\b(\w+-?\w+)\b", product))
-    for uri,name in category_names.items():
-        category_words = name.split()
-        category_score = 0
-        for category_word in category_words:
-            if(category_word.lower() in " ".join(cluster_keywords).lower()):
-                category_score +=len(category_word)/len(name)
-        category_scores.append((category_score, name,uri))
+        print("no selection")
+    for uri, name in category_paths.items():  # test for whole paths
+        category_score = get_category_score(name, category_keywords)
+        category_scores.append((category_score, name, uri))
     top_categories = sorted(category_scores, key=lambda x: x[0], reverse=True)
+    print("\n")
+    print("Products to receive categories")
     for text in products["product_titles"][0:3]:
         print(text)
-    for text in top_categories[0:5]:
-        print(text[1])
-    input("Check paths and assign if relation is found")
-# for each cluster find words that relates the products to a category
-
-
-for attribute in attributes:
-    if attribute in category_names:
-        print(attribute)
-
-matching_categories = {}
-
-# Create a regex pattern to match any string in the first array
-for attribute in attributes:
-    if len(attribute.split()) > 1:
-        pattern = "|".join(map(re.escape, attribute.split()))
+    print("\n")
+    print("Category matches")
+    for i, text in enumerate(top_categories[0:5]):
+        print("\n")
+        print(f"{i}:{text[1]}:({category_paths[text[2]]})")
+    print("\n")
+    selection = input("Check paths and assign if relation is found[0-4]")
+    if selection != "" and int(selection) in range(0, 5):
+        print(f"selection {top_categories[int(selection)]}")
+        query = update_product_google_categories_query(
+            product_uris, top_categories[int(selection)][2]
+        )
+        # print(query)
+        # virtuoso.get(query)
+        # update product google categories
+        continue
+        # exit()
     else:
-        pattern = attribute
-    # Combine the pattern using the '|' (OR) operator
-    regex = re.compile(pattern)
-    # Check if any category in the second array matches the regex pattern
-    matches = [category for category in category_names if regex.search(category)]
-    if len(matches) > 0:
-        matching_categories[attribute] = matches
+        print("no selection")
+    remaining_clusters[i] = products
+
+print("Manual phase")
+print(remaining_clusters)
+# Custom category assignment
+for cluster in remaining_clusters:
+    product_uris = [
+        product_uri
+        for product_uris in products["product_uris"]
+        for product_uri in product_uris
+    ]
+    print("Products to receive categories")
+    for text in products["product_titles"][0:3]:
+        print(text)
+    print("\n")
+    category_id = input(
+        "Give the id of the google category from the txt file for assignment(ID - Category > Path)"
+    )
+    if category_id != "":
+        category_uri = (
+            f"http://magelon.com/ontologies/google_categories/id={category_id}"
+        )
+        query = update_product_google_categories_query(product_uris, category_uri)
+        virtuoso.get(query)
